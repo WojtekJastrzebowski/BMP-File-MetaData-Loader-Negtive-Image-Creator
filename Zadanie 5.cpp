b030407eb18cef09e7ca773bef27f0fb78a4fd34
Wojtek Jastrzebowski <jastrzebowskiweb@gmail.com>
2022-06-24 21:59:21 +0200

    kolejne zmiany

--- Zadanie 5.cpp
+++ Zadanie 5.cpp
@@ -99,5 +99,44 @@ int main(int arc, char* argv[]) {
     printf("\n Wazne kolory w palecie: %d", Picture.biClrImportant);
 
 
+    FILE* w = fopen("negative.bmp", "wb");
+    if (w == NULL)
+    {
+        printf("\n\n Can't open the file");
+        return -1;
+    }
+    else
+    {
+        printf("\n\n File w opened!");
+    }
+
+    fseek(w, 0, SEEK_SET);
+    fwrite(&File.bfType, sizeof(File.bfType), 1, w);
+    fwrite(&File.bfSize, sizeof(File.bfSize), 1, w);
+    fwrite(&File.bfReserved1, sizeof(File.bfReserved1), 1, w);
+    fwrite(&File.bfReserved2, sizeof(File.bfReserved2), 1, w);
+    fwrite(&File.bfOffBits, sizeof(File.bfOffBits), 1, w);
+
+    fseek(w, 14, SEEK_SET);
+    fwrite(&Picture.biSize, sizeof(Picture.biSize), 1, w);
+    fwrite(&Picture.biWidth, sizeof(Picture.biWidth), 1, w);
+    fwrite(&Picture.biHeight, sizeof(Picture.biHeight), 1, w);
+    fwrite(&Picture.biPlanes, sizeof(Picture.biPlanes), 1, w);
+    fwrite(&Picture.biBitCount, sizeof(Picture.biBitCount), 1, w);
+    fwrite(&Picture.biCompression, sizeof(Picture.biCompression), 1, w);
+    fwrite(&Picture.biSizeImage, sizeof(Picture.biSizeImage), 1, w);
+    fwrite(&Picture.biXPelsPerMeter, sizeof(Picture.biXPelsPerMeter), 1, w);
+    fwrite(&Picture.biYPelsPerMeter, sizeof(Picture.biYPelsPerMeter), 1, w);
+    fwrite(&Picture.biClrUsed, sizeof(Picture.biClrUsed), 1, w);
+    fwrite(&Picture.biClrImportant, sizeof(Picture.biClrImportant), 1, w);
+
+    fseek(w, sizeof(File.bfOffBits), SEEK_SET);
+
+    
+
+    printf("\n");
+    fclose(f);
+    fclose(w);
+
     return 0;
 }
