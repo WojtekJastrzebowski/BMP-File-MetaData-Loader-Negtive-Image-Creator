f0acdd6153768a8e40dfe4052cfd26139219356b
Wojtek Jastrzebowski <jastrzebowskiweb@gmail.com>
2022-06-24 21:50:56 +0200

    dodanie struktur

--- Zadanie 5.cpp
+++ Zadanie 5.cpp
@@ -1,1 +1,32 @@
-﻿
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+struct FileHeader {
+    short bfType;
+    int bfSize;
+    short bfReserved1;
+    short bfReserved2;
+    int bfOffBits;
+} File;
+
+struct PictureHeader {
+    int biSize;
+    int biWidth;
+    int biHeight;
+    short biPlanes;
+    short biBitCount;
+    int biCompression;
+    int biSizeImage;
+    int biXPelsPerMeter;
+    int biYPelsPerMeter;
+    int biClrUsed;
+    int biClrImportant;
+} Picture;
+
+struct KoloryRGB {
+    char R;
+    char G;
+    char B;
+} Rgb;
