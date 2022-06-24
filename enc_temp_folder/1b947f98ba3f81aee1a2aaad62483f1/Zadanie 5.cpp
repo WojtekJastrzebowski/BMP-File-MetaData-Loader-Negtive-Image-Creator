bb8a713abdaa8f20ca5a17a1140e1630a8314e3c
Wojtek Jastrzebowski <jastrzebowskiweb@gmail.com>
2022-06-24 21:58:07 +0200

    wprowadzanie kolejnych funkcji

--- enc_temp_folder/1b947f98ba3f81aee1a2aaad62483f1/Zadanie 5.cpp
+++ enc_temp_folder/1b947f98ba3f81aee1a2aaad62483f1/Zadanie 5.cpp
@@ -98,6 +98,52 @@ int main(int arc, char* argv[]) {
     fread(&Picture.biClrImportant, sizeof(Picture.biClrImportant), 1, f);
     printf("\n Wazne kolory w palecie: %d", Picture.biClrImportant);
 
+    FILE* w = fopen("negative.bmp", "wb");
+    if (w == NULL)
+    {
+        printf("\n\n Can't open the file");
+        return -1;
+    }
+    else
+    {
+        printf("\n\n File w opened!");
+    }
+
+    fseek(w, 0, SEEK_SET);
+    fwrite(&File.bfType, sizeof(File.bfType), 1, w);
+    fwrite(&File.bfSize, sizeof(File.bfSize), 1, w);
+    fwrite(&File.bfReserved1, sizeof(File.bfReserved1), 1, w);
+    fwrite(&File.bfReserved2, sizeof(File.bfReserved2), 1, w);
+    fwrite(&File.bfOffBits, sizeof(File.bfOffBits), 1, w);
+
+    fseek(w, 14, SEEK_SET);
+    fwrite(&Picture.biSize, sizeof(Picture.biSize), 1, w);
+    fwrite(&Picture.biWidth, sizeof(Picture.biWidth), 1, w);
+    fwrite(&Picture.biHeight, sizeof(Picture.biHeight), 1, w);
+    fwrite(&Picture.biPlanes, sizeof(Picture.biPlanes), 1, w);
+    fwrite(&Picture.biBitCount, sizeof(Picture.biBitCount), 1, w);
+    fwrite(&Picture.biCompression, sizeof(Picture.biCompression), 1, w);
+    fwrite(&Picture.biSizeImage, sizeof(Picture.biSizeImage), 1, w);
+    fwrite(&Picture.biXPelsPerMeter, sizeof(Picture.biXPelsPerMeter), 1, w);
+    fwrite(&Picture.biYPelsPerMeter, sizeof(Picture.biYPelsPerMeter), 1, w);
+    fwrite(&Picture.biClrUsed, sizeof(Picture.biClrUsed), 1, w);
+    fwrite(&Picture.biClrImportant, sizeof(Picture.biClrImportant), 1, w);
+
+    fseek(w, sizeof(File.bfOffBits), SEEK_SET);
+
+    int bmpImg;
+    for (int i = File.bfOffBits; i < File.bfSize; i++)
+    {
+        fseek(f, i, SEEK_SET);
+        fseek(w, i, SEEK_SET);
+        fread(&bmpImg, 3, 1, f);
+        bmpImg = INT_MAX - bmpImg; //Tworzymy negatyw
+        fwrite(&bmpImg, 3, 1, w);
+    }
+
+    printf("\n");
+    fclose(f);
+    fclose(w);
 
     return 0;
 }
