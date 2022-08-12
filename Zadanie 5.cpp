d6ec4e81f1c092fa039b49dfe014591e45fa99f9
WojtekJastrzebowski <102751118+WojtekJastrzebowski@users.noreply.github.com>
2022-08-12 11:45:29 +0200

    Update Zadanie 5.cpp

--- Zadanie 5.cpp
+++ Zadanie 5.cpp
@@ -43,60 +43,60 @@ int main(int arc, char* argv[]) {
     }
     else
     {
-        printf("\n\n File f opened!");
+        printf("\n\n File .BMP opened!");
     }
-    printf("\n INFORMACJE O BITMAPIE\n\n");
+    printf("\n BITMAP INFO\n\n");
 
     fread(&File.bfType, sizeof(File.bfType), 1, f);
-    printf(" Typ: %x", File.bfType);
+    printf(" Type: %x", File.bfType);
 
     fread(&File.bfSize, sizeof(File.bfSize), 1, f);
-    printf("\n Rozmiar pliku: %d bajtow", File.bfSize);
+    printf("\n File size: %d bajtow", File.bfSize);
 
     fread(&File.bfReserved1, sizeof(File.bfReserved1), 1, f);
-    printf("\n Zarezerwowane1: %d", File.bfReserved1);
+    printf("\n Reserved 1: %d", File.bfReserved1);
 
     fread(&File.bfReserved2, sizeof(File.bfReserved2), 1, f);
-    printf("\n Zarezerwowane2: %d", File.bfReserved2);
+    printf("\n Reserved 2: %d", File.bfReserved2);
 
     fread(&File.bfOffBits, sizeof(File.bfOffBits), 1, f);
-    printf("\n Pozycja danych obrazkowych: %d", File.bfOffBits);
+    printf("\n File OffBits: %d", File.bfOffBits);
 
     printf("\n");
 
     fseek(f, 14, SEEK_SET);
     fread(&Picture.biSize, sizeof(Picture.biSize), 1, f);
-    printf("\n Wielkosc naglowka informacyjnego: %d", Picture.biSize);
+    printf("\n Size of information header: %d", Picture.biSize);
 
     fread(&Picture.biWidth, sizeof(Picture.biWidth), 1, f);
-    printf("\n Szerokosc: %d pikseli", Picture.biWidth);
+    printf("\n Width: %d pixels", Picture.biWidth);
 
     fread(&Picture.biHeight, sizeof(Picture.biHeight), 1, f);
-    printf("\n Wysokosc: %d pikseli", Picture.biHeight);
+    printf("\n Height: %d pixels", Picture.biHeight);
 
     fread(&Picture.biPlanes, sizeof(Picture.biPlanes), 1, f);
-    printf("\n Liczba platow: %d", Picture.biPlanes);
+    printf("\n Planes: %d", Picture.biPlanes);
 
     fread(&Picture.biBitCount, sizeof(Picture.biBitCount), 1, f);
-    printf("\n Liczba bitow na piksel: %d (1, 4, 8, or 24)", Picture.biBitCount);
+    printf("\n Bit Count for pixel: %d (1, 4, 8, or 24)", Picture.biBitCount);
 
     fread(&Picture.biCompression, sizeof(Picture.biCompression), 1, f);
-    printf("\n Kompresja: %d (0=none, 1=RLE-8, 2=RLE-4)", Picture.biCompression);
+    printf("\n Compression: %d (0=none, 1=RLE-8, 2=RLE-4)", Picture.biCompression);
 
     fread(&Picture.biSizeImage, sizeof(Picture.biSizeImage), 1, f);
-    printf("\n Rozmiar samego rysunku: %d", Picture.biSizeImage);
+    printf("\n Size Image Only: %d", Picture.biSizeImage);
 
     fread(&Picture.biXPelsPerMeter, sizeof(Picture.biXPelsPerMeter), 1, f);
-    printf("\n Rozdzielczosc pozioma: %d", Picture.biXPelsPerMeter);
+    printf("\n Horizontal resolution: %d", Picture.biXPelsPerMeter);
 
     fread(&Picture.biYPelsPerMeter, sizeof(Picture.biYPelsPerMeter), 1, f);
-    printf("\n Rozdzielczosc pionowa: %d", Picture.biYPelsPerMeter);
+    printf("\n Vertical resolution: %d", Picture.biYPelsPerMeter);
 
     fread(&Picture.biClrUsed, sizeof(Picture.biClrUsed), 1, f);
-    printf("\n Liczba kolorow w palecie: %d", Picture.biClrUsed);
+    printf("\n Number of colors in the palette: %d", Picture.biClrUsed);
 
     fread(&Picture.biClrImportant, sizeof(Picture.biClrImportant), 1, f);
-    printf("\n Wazne kolory w palecie: %d", Picture.biClrImportant);
+    printf("\n Important colors in the palette: %d", Picture.biClrImportant);
 
 
     FILE* w = fopen("negative.bmp", "wb");
@@ -138,7 +138,7 @@ int main(int arc, char* argv[]) {
         fseek(f, i, SEEK_SET);
         fseek(w, i, SEEK_SET);
         fread(&bmpImg, 3, 1, f);
-        bmpImg = INT_MAX - bmpImg; //Tworzymy negatyw
+        bmpImg = INT_MAX - bmpImg; //Creating Negative 
         fwrite(&bmpImg, 3, 1, w);
     }
 
